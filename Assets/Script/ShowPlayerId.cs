8b9c3351e742f946d3d7333374057c455cfc8659
Marta <martacorcolesvalle@gmail.com>
2020-02-19 22:41:02 +0100

    first commit

---
+++ Assets/Script/ShowPlayerId.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPlayerId : MonoBehaviour
+{
+    // Cached references
+    [SerializeField] public GameManager gameManager;
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        gameObject.GetComponent<Text>().text = gameManager.PlayerId;
+    }
+}
